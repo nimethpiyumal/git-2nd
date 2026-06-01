773c6dc12a93af60b183181bd268fdd21a15f805
deshan <deshanrashmika589@gmail.com>
2026-06-01 09:20:05 +0530

    initial commit

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ int main() {
     float celsius, fahrenheit;
 
 
-    printf("Enter temperature in Centigrade: ");
+    printf("Enter temperature in Centigrade:\n ");
     scanf("%f", &celsius);
 
 
@@ -12,6 +12,13 @@ int main() {
 
 
     printf("%.2f Centigrade is equal to %.2f Fahrenheit.\n", celsius, fahrenheit);
+   if(fahrenheit>=86){
+    printf("\nToday is hot-You must drink water well\n");
+   }
+   else {
+    printf("\nToday is cool\n");
+   }
+    printf("\n Have a great day\n");
 
     return 0;
 }
